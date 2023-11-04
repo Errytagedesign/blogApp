86b3cbfa7457e9714c19d6f29e5a21eb779b0076
Errytagedesign <errytage@gmail.com>
2023-11-04 12:24:03 +0100

    Integrate standard layout in our controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-  def index; end
+  layout 'standard'
+  def index
+    # retrieves the user using their id
+    @user = User.find(params[:user_id])
+    # Get the post associated with the above user, where the authorId matches the user id above
+    @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
+    # paginate post by showing two at a go
+    @posts = @posts.paginate(page: params[:page], per_page: 2)
+  end
 
-  def show; end
+  # an action to render specific post using the post id
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    # render user based on id in ascending order
+    @user = User.order(id: :asc)
+end
 
-  def show; end
+# render specific user based on id
+  def show
+    @user = User.find(params:[:id])
+   end
 end
