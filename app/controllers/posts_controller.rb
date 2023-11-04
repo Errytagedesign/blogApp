b081a7da885bad0648845dc8de853b079908fbfd
Errytagedesign <errytage@gmail.com>
2023-11-04 18:06:33 +0100

    Add route for new post and comments, add form for new post and current user method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,27 @@ class PostsController < ApplicationController
     @posts = @posts.paginate(page: params[:page], per_page: 2)
   end
 
-  # an action to render specific post using the post
+  # an action to render specific post using the post id
   def show
     @post = Post.find(params[:id])
   end
+
+  # Render a form to create new post
+  def new
+    @post = Post.new
+  end
+
+  # Initialize submission of new post to create new Post using the Post.new object
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post Created!'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post can not be created!'
+      render :new, locals: { post: @post }
+    end
+  end
+
 end
