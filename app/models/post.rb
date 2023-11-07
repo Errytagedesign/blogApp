ae66b7f66147ab17b322541a33755b0099e7bd70
Errytagedesign <errytage@gmail.com>
2023-11-07 15:00:40 +0100

    Make queries more coincise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
 class Post < ApplicationRecord
   has_many :likes
   has_many :comments
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :update_post_counter
 
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.limit(5).order(created_at: :desc)
   end
 
   private
@@ -18,4 +17,7 @@ class Post < ApplicationRecord
   def update_post_counter
     author.increment!(:posts_counter)
   end
+
+  # pagination
+  self.per_page = 10
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    # return the first three post
+    posts.order(created_at: :desc).first(3)
   end
 end
